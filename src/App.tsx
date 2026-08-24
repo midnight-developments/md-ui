72ecae10247c6a1908dcbb1887d57e1acf13d54e
danify21 <danyalthebest74@gmail.com>
2026-08-24 18:01:58 -0400

    implemented zod validation with react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { CodeBracketIcon, ShareIcon } from '@heroicons/react/24/solid'
+import { ShareIcon } from '@heroicons/react/24/solid'
 import {
     Card,
     CardHeader,
@@ -15,106 +15,139 @@ import {
 import { Input } from './components/ui/input'
 import { Button } from "./components/ui/button"
 import { Field, FieldLabel, FieldDescription, FieldGroup, FieldSeparator, FieldSet } from './components/ui/field'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const shareFormSchema = z.object({
+    expirationDays: z.number({ message: "Expiration period must be a number." })
+        .min(1, { message: "Expiration period must be at least 1 day." }),
+    passcode: z.string()
+        .min(4, { message: "Passcode must be at least 4 characters." })
+        .or(z.literal(""))
+})
+
+type ShareFormValues = z.infer<typeof shareFormSchema>
 
 export default function App() {
     const [isOpen, setIsOpen] = useState(false)
-    const [expirationDays, setExpirationDays] = useState(30)
-    const [passcode, setPasscode] = useState("")
 
-    const passcodeError = passcode.length > 0 && passcode.length < 4
-        ? "Passcode must be at least 4 characters."
-        : undefined
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm<ShareFormValues>({
+        resolver: zodResolver(shareFormSchema),
+        defaultValues: {
+            expirationDays: 30,
+            passcode: "",
+        },
+        mode: "onChange",
+    })
 
-    const expirationError = expirationDays < 1
-        ? "Expiration period must be at least 1 day."
-        : undefined
+    const onSubmit = (data: ShareFormValues) => {
+        console.log("Form Submitted:", data)
+        setIsOpen(false)
+        reset()
+    }
 
     return (
         <div className="min-h-screen flex items-center justify-center">
             <Button onClick={() => setIsOpen(true)}>Open Share Panel</Button>
 
-            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <Dialog open={isOpen} onOpenChange={(open) => {
+                setIsOpen(open)
+                if (!open) {
+                    reset()
+                }
+            }}>
                 <DialogContent>
-                    <Card className="w-md">
-                        <CardHeader className="flex flex-row items-center gap-3">
-                            <CardTitleIcon icon={ShareIcon} />
-                            <CardTitle>Share Project</CardTitle>
-
-                        </CardHeader>
-                        <CardContent>
-                            <FieldSet>
-                                <FieldSeparator className="my-0" />
-                                <FieldGroup>
-                                    <Field>
-                                        <FieldLabel>Document Link</FieldLabel>
-                                        <div className="flex flex-row items-center gap-2 w-full">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Card className="w-md">
+                            <CardHeader className="flex flex-row items-center gap-3">
+                                <CardTitleIcon icon={ShareIcon} />
+                                <CardTitle>Share Project</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <FieldSet>
+                                    <FieldSeparator className="my-0" />
+                                    <FieldGroup>
+                                        <Field>
+                                            <FieldLabel>Document Link</FieldLabel>
+                                            <div className="flex flex-row items-center gap-2 w-full">
+                                                <Input
+                                                    value='https://docs.google.com/document/d/1234567890/edit?usp=sharing'
+                                                    readOnly
+                                                />
+                                                <Button
+                                                    type="button"
+                                                    variant='outline'
+                                                    onClick={() => {
+                                                        navigator.clipboard.writeText('https://docs.google.com/document/d/1234567890/edit?usp=sharing')
+                                                    }}
+                                                >
+                                                    Copy
+                                                </Button>
+                                            </div>
+                                            <FieldDescription description="Share this link with others" />
+                                        </Field>
+                                        <Field>
+                                            <FieldLabel>Search Recipents</FieldLabel>
+                                            <div className="flex flex-row items-center gap-2 w-full">
+                                                <Input placeholder='' />
+                                                <Button type="button" variant='outline'>Invite</Button>
+                                            </div>
+                                            <FieldDescription description="Add collaborators by username" />
+                                        </Field>
+                                    </FieldGroup>
+                                    <FieldSeparator />
+                                    <FieldGroup>
+                                        <Field data-invalid={!!errors.expirationDays}>
+                                            <FieldLabel>Expiration Period</FieldLabel>
                                             <Input
-                                                value='https://docs.google.com/document/d/1234567890/edit?usp=sharing'
-                                                readOnly
+                                                type="number"
+                                                {...register('expirationDays', { valueAsNumber: true })}
                                             />
-                                            <Button
-                                                variant='outline'
-                                                onClick={() => {
-                                                    navigator.clipboard.writeText('https://docs.google.com/document/d/1234567890/edit?usp=sharing')
-                                                }}
-                                            >
-                                                Copy
-                                            </Button>
-                                        </div>
-                                        <FieldDescription description="Share this link with others" />
-                                    </Field>
-                                    <Field>
-                                        <FieldLabel>Search Recipents</FieldLabel>
-                                        <div className="flex flex-row items-center gap-2 w-full">
-                                            <Input placeholder='' />
-                                            <Button variant='outline'>Invite</Button>
-                                        </div>
-                                        <FieldDescription description="Add collaborators by username" />
-                                    </Field>
-                                </FieldGroup>
-                                <FieldSeparator />
-                                <FieldGroup>
-                                    <Field data-invalid={!!expirationError}>
-                                        <FieldLabel>Expiration Period</FieldLabel>
-                                        <Input
-                                            type="number"
-                                            value={expirationDays}
-                                            onChange={(e) => setExpirationDays(Number(e.target.value))}
-                                        />
-                                        <FieldDescription
-                                            description="Number of days before the link expires"
-                                            error={expirationError}
-                                        />
-                                    </Field>
-                                    <Field data-invalid={!!passcodeError}>
-                                        <FieldLabel>Access Passcode</FieldLabel>
-                                        <Input
-                                            type="password"
-                                            value={passcode}
-                                            onChange={(e) => setPasscode(e.target.value)}
-                                            placeholder="Enter passcode"
-                                        />
-                                        <FieldDescription
-                                            description="Require visitors to enter this passcode to view"
-                                            error={passcodeError}
-                                        />
-                                    </Field>
-                                </FieldGroup>
-                                <FieldSeparator />
-
-                            </FieldSet>
-                        </CardContent>
-                        <CardFooter className="justify-between">
-                            <div className="flex items-center gap-2">
-
-
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <Button variant='outline' onClick={() => setIsOpen(false)}>Cancel</Button>
-                                <Button variant='default' onClick={() => setIsOpen(false)}>Confirm Access</Button>
-                            </div>
-                        </CardFooter>
-                    </Card>
+                                            <FieldDescription
+                                                description="Number of days before the link expires"
+                                                error={errors.expirationDays?.message}
+                                            />
+                                        </Field>
+                                        <Field data-invalid={!!errors.passcode}>
+                                            <FieldLabel>Access Passcode</FieldLabel>
+                                            <Input
+                                                type="password"
+                                                placeholder="Enter passcode"
+                                                {...register('passcode')}
+                                            />
+                                            <FieldDescription
+                                                description="Require visitors to enter this passcode to view"
+                                                error={errors.passcode?.message}
+                                            />
+                                        </Field>
+                                    </FieldGroup>
+                                    <FieldSeparator />
+                                </FieldSet>
+                            </CardContent>
+                            <CardFooter className="justify-between">
+                                <div className="flex items-center gap-2" />
+                                <div className="flex items-center gap-2">
+                                    <Button
+                                        type="button"
+                                        variant='outline'
+                                        onClick={() => {
+                                            setIsOpen(false)
+                                            reset()
+                                        }}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button type="submit" variant='default'>Confirm Access</Button>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </form>
                 </DialogContent>
             </Dialog>
         </div>
